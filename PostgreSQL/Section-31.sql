81a37ab10bc2ff2cf57a4fd317a796566189257a
dhasanuja <anujadhas08@gmail.com>
2022-01-20 21:13:12 +0530

    section 31

---
+++ PostgreSQL/Section-31.sql
@@ -0,0 +1,17 @@
+---section 31
+CREATE DATABASE northwind2;
+
+createdb fiddlesticks
+
+
+CREATE DATABASE mydb5;
+
+createdb mydb5_bak
+-----drop
+DROP DATABASE IF EXISTS northwind2;
+
+dropdb fiddlesticks
+
+DROP DATABASE IF EXISTS mydb5;
+
+dropdb mydb5_bak
